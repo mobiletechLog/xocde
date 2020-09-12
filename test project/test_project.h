505bf6d547b163315dec9ea9352235f0abee6436
user <mazur.pro20@gmail.com>
2020-09-13 00:00:03 +0300

    first project

---
+++ test project/test_project.h
@@ -0,0 +1,19 @@
+//
+//  test_project.h
+//  test project
+//
+//  Created by user on 9/9/20.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for test_project.
+FOUNDATION_EXPORT double test_projectVersionNumber;
+
+//! Project version string for test_project.
+FOUNDATION_EXPORT const unsigned char test_projectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <test_project/PublicHeader.h>
+
+
